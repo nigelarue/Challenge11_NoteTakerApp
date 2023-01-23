03fb18dfc5cc49c9f1193ab3155dc6fa662105f1
nigelarue <larue.nigel91@gmail.com>
2023-01-23 15:41:56 -0600

    created PORT, express middleware functions, and imports from routes directory.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+// set routes 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Sets server to start via the assigned port.
+app.listen(PORT, () => console.log(`ATTENTION: listening at PORT ${PORT}`));
